ab6469601fe98a69ad813308f15a8730a2fc6ae1
trogglebyte06 <henryr.thomas@gmail.com>
2025-03-15 21:57:41 -0500

    exercises

--- ex2_3.cpp
+++ ex2_3.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	//unsigned u = 10, u2 = 42;
+	//std::cout << u2 - u << std::endl;
+	//std::cout << u - u2 << std::endl;
+
+	//int i = 10, i2 = 42;
+	//std::cout << i2 - i << std::endl;
+	//std::cout << i - i2 << std::endl;
+
+	//std::cout << i - u << std::endl;
+	//std::cout << u - i << std::endl;
+	
+	int oct = 019;
+	std::cout << oct + 1 << std::endl;
+
 	return 0;
 }
